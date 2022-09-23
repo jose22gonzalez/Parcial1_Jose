d8709aa89a9ec610f68dc4341362ed5db96f4321
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-09-22 22:27:11 -0400

    Creando la entidad

---
+++ app/src/main/java/edu/ucne/parcial1_jose/data/entity/Parcial1entity.kt
@@ -0,0 +1,14 @@
+package edu.ucne.parcial1_jose.data.entity
+
+import androidx.room.Dao
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Dao
+@Entity(tableName = "Parcial1")
+data class Examenentity (
+    @PrimaryKey(autoGenerate = true)
+    val parcialId: Int = 0,
+    val descripcion: String = "",
+    val nota: Double
+)
