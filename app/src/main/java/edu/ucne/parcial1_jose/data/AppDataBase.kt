1cb85b6ab0a1ff06dc468d9c49192032cd9aec2f
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-09-24 09:03:40 -0400

    Arreglando la Base de Dato

--- app/src/main/java/edu/ucne/parcial1_jose/data/AppDataBase.kt
+++ app/src/main/java/edu/ucne/parcial1_jose/data/AppDataBase.kt
@@ -13,25 +13,5 @@ import edu.ucne.parcial1_jose.data.entity.Examenentity
 )
 abstract class ExamenDatabase: RoomDatabase(){
 
-    companion object {
-        @Volatile
-        private var INSTANCE: ExamenDatabase? = null
-        fun getInstance(context: Context): ExamenDatabase {
-            synchronized(this) {
-                var instance = INSTANCE
 
-                if(instance == null) {
-                    instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        ExamenDatabase::class.java,
-                        "Examen1"
-                    )
-                        .fallbackToDestructiveMigration()
-                        .build()
-                    INSTANCE = instance
-                }
-                return instance
-            }
-        }
-    }
 }
