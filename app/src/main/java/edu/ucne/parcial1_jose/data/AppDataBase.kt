2b10d7fd5852a04e81cfaddafc62f628f83fc59f
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-09-28 18:24:10 -0400

    Renombrando y  creando una variable abstracta de tipo Dao

--- app/src/main/java/edu/ucne/parcial1_jose/data/AppDataBase.kt
+++ app/src/main/java/edu/ucne/parcial1_jose/data/AppDataBase.kt
@@ -2,13 +2,15 @@ package edu.ucne.parcial1_jose.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import edu.ucne.parcial1_jose.data.entity.Examenentity
+import edu.ucne.parcial1_jose.data.dao.ArticuloDao
+import edu.ucne.parcial1_jose.data.entity.Articuloentity
+
 
 @Database(
-    entities = [Examenentity::class],
+    entities = [Articuloentity::class],
     version = 2,
     exportSchema = false
 )
 abstract class ExamenDatabase: RoomDatabase(){
-
+    abstract val ArticuloDao: ArticuloDao
 }
