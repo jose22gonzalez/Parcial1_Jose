b9b05ad5be048414e00b9433bcbf8edad4ef2117
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-09-28 18:09:40 -0400

    Cambiando la version

--- app/src/main/java/edu/ucne/parcial1_jose/data/AppDataBase.kt
+++ app/src/main/java/edu/ucne/parcial1_jose/data/AppDataBase.kt
@@ -1,17 +1,14 @@
 package edu.ucne.parcial1_jose.data
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import edu.ucne.parcial1_jose.data.entity.Examenentity
 
 @Database(
     entities = [Examenentity::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class ExamenDatabase: RoomDatabase(){
-
 
 }
