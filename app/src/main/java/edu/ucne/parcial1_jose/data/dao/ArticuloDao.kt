e8abb92e013081728d093749d0502e9610f806a7
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-09-28 18:21:54 -0400

    Creando la Dao de Articulo

---
+++ app/src/main/java/edu/ucne/parcial1_jose/data/dao/ArticuloDao.kt
@@ -0,0 +1,26 @@
+package edu.ucne.parcial1_jose.data.dao
+
+import androidx.room.*
+import edu.ucne.parcial1_jose.data.entity.Articuloentity
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface ArticuloDao {
+
+    @Insert
+    suspend fun InsertaArticulo(articuloentity: Articuloentity)
+
+    @Update
+    suspend fun ActualizarArticulo(articuloentity: Articuloentity)
+
+    @Delete
+    suspend fun EliminarArticulo(articuloentity: Articuloentity)
+
+    @Query("SELECT * FROM Articulos WHERE ArticuloId = :id")
+    fun FindArticulo(id: Int): Flow<Articuloentity>
+
+    @Query("SELECT * FROM Articulos")
+    fun ListArticulo(): Flow<List<Articuloentity>>
+
+}
