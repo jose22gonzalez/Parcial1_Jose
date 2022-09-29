0689e2cf0f7a75a9d41e8411f70f9d1e04433141
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-09-28 20:00:28 -0400

    Modificando la Navegacion

--- app/src/main/java/edu/ucne/parcial1_jose/ui/navigation/Navigation.kt
+++ app/src/main/java/edu/ucne/parcial1_jose/ui/navigation/Navigation.kt
@@ -1,15 +1,10 @@
 package edu.ucne.parcial1_jose.ui.navigation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import edu.ucne.parcial1_jose.ui.Pantalla1Screen.PantallaList.PantallalistScreen
+import edu.ucne.parcial1_jose.ui.Pantalla1Screen.PantallaList.ArticuloListScreen
 import edu.ucne.parcial1_jose.ui.Pantalla1Screen.PantallaScreen.PantallaScreen
 
 
@@ -18,7 +13,7 @@ fun Navigation(navController: NavHostController) {
 
     NavHost(navController = navController, startDestination = "PantallaListScreen"){
         composable("PantallaListScreen"){
-            PantallalistScreen(onClick = {navController.navigate("PantallaScreen")})
+            ArticuloListScreen(onClick = {navController.navigate("PantallaScreen")})
         }
 
         composable("PantallaScreen"){
